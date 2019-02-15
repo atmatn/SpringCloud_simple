c9f4dfc2424ffd575f1ef29806df76e195d7d8e1
gaocheng <gaocheng@rd.netease.com>
2019-02-15 12:28:15 +0800

    完成了简单的zuul前置路由过滤

--- testproject/test-zuul-port-8089/src/main/java/com/youdao/crop/Zuul8089Application.java
+++ testproject/test-zuul-port-8089/src/main/java/com/youdao/crop/Zuul8089Application.java
@@ -1,13 +1,19 @@
 package com.youdao.crop;
 
+import com.youdao.crop.controller.TestFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringCloudApplication
 public class Zuul8089Application {
     public static void main(String[] args) {
         SpringApplication.run(Zuul8089Application.class,args);
+    }
+    @Bean
+    public TestFilter testFilter(){//重要一步，实例化我们自定义的过滤器
+        return new TestFilter();
     }
 }
---
+++ testproject/test-zuul-port-8089/src/main/java/com/youdao/crop/controller/TestFilter.java
@@ -0,0 +1,49 @@
+package com.youdao.crop.controller;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class TestFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        /*pre：可以在请求被路由之前调用
+	    route：在路由请求时候被调用
+	    post：在route和error过滤器之后被调用
+	    error：处理请求时发生错误时被调用*/
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() { //过滤器优先级 数值越小，优先级越高
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {//是否执行该过滤器
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(String.format("%s demoFilter request to %s", request.getMethod(), request.getRequestURL().toString()));
+        String username = request.getParameter("username");// 获取请求的参数
+        if(!StringUtils.isEmpty(username)&&username.equals("gaocheng")){//通过
+            ctx.setSendZuulResponse(true);// 对该请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);// 设值，让下一个Filter看到上一个Filter的状态
+            return null;
+        }else{
+            ctx.setSendZuulResponse(false);// 过滤该请求，不对其进行路由
+            ctx.setResponseStatusCode(401);// 返回错误码
+            ctx.setResponseBody("{\"result\":\"Sorry the username is not correct!\"}");// 返回错误内容
+            ctx.set("isSuccess", false);
+            return null;
+        }
+    }
+}
