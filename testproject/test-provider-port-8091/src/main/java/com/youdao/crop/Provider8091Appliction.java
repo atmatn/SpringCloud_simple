75ad2da5c5af41ba0325f31edb33862cd6c0440c
gaocheng <gaocheng@rd.netease.com>
2019-02-20 19:51:15 +0800

    完成简单的config配置

--- testproject/test-provider-port-8091/src/main/java/com/youdao/crop/Provider8091Appliction.java
+++ testproject/test-provider-port-8091/src/main/java/com/youdao/crop/Provider8091Appliction.java
@@ -1,13 +1,26 @@
 package com.youdao.crop;
 
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class Provider8091Appliction {
     public static void main(String[] args) {
-        SpringApplication.run(Provider8091Appliction.class,args);
+        SpringApplication.run(Provider8091Appliction.class, args);
     }
+//
+//    @Value("${application}")
+//    String hello;
+//
+//    @RequestMapping(value = "/config", method = RequestMethod.GET)
+//    public String getConfig() {
+//        return hello;
+//    }
 }
